294382d01083ace044aa985be4ad2d5df4f93587
pelo0819 <canvas.pelo@gmail.com>
2021-11-28 00:20:39 +0900

    update

--- java/tests/proto/servers/DbManager.java
+++ java/tests/proto/servers/DbManager.java
@@ -18,13 +18,10 @@ public class DbManager
     {
         try
         {
-            // Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://192.168.3.7:3306/CANVAS_DB";
-            // String url = "jdbc:mysql://192.168.3.7:3306/CANVAS_DB?user=pelo&password=0819Tobita";
-            String user = "pelo";
-            String password = "0819Tobita";
+            String user = "";
+            String password = "";
             con = DriverManager.getConnection(url, user, password);
-            // con = DriverManager.getConnection(url);
 
             String sql = "select * from user";
             pstmt = con.prepareStatement(sql);
